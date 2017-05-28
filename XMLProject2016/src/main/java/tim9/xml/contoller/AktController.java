86b1cbe026458b539d3af144cd65f8e6483311fd
Ana Marojevic <ana94ns@hotmail.com>
2017-05-28 19:55:07 +0200

    Added project again

---
+++ XMLProject2016/src/main/java/tim9/xml/contoller/AktController.java
@@ -0,0 +1,31 @@
+package tim9.xml.contoller;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.xml.bind.JAXBException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import tim9.xml.xquery.Util;
+import tim9.xml.xquery.XMLReader;
+import tim9.xml.xquery.XMLWriter;
+
+@Controller
+@RequestMapping(value="xml/akt")
+public class AktController {
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void saveAkt() throws FileNotFoundException, IOException{
+		System.out.println("OVDE");
+		XMLWriter.run(Util.loadProperties(), ".ana", "./data/akt1.xml");
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public void getAkt() throws FileNotFoundException, IOException, JAXBException{
+		XMLReader.run(Util.loadProperties(), ".asa");
+	}
+
+}
---
+++ XMLProject2016/src/main/java/tim9/xml/xquery/Util.java
@@ -0,0 +1,89 @@
+package tim9.xml.xquery;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.marklogic.client.DatabaseClientFactory.Authentication;
+
+public class Util {
+	
+	static public class ConnectionProperties {
+
+		public String host;
+		public int port = -1;
+		public String user;
+		public String password;
+		public String database;
+		public String proxyHost;
+		public String proxyPort;
+		public boolean usingProxy;
+		public Authentication authType;
+
+		public ConnectionProperties(Properties props) {
+			super();
+			host = props.getProperty("conn.host").trim();
+			port = Integer.parseInt(props.getProperty("conn.port"));
+			user = props.getProperty("conn.user").trim();
+			password = props.getProperty("conn.password").trim();
+			database = props.getProperty("conn.database").trim();
+			//proxyHost = props.getProperty("conn.proxy_host").trim();
+			//proxyPort = props.getProperty("conn.proxy_port").trim();
+			//usingProxy = Boolean.parseBoolean(props.getProperty("conn.using_proxy").trim());
+			authType = Authentication.valueOf(props.getProperty("conn.authentication_type").toUpperCase().trim());
+			
+			// Proxy settings
+//			if (usingProxy) {
+//				setProxy(proxyHost, proxyPort);
+//			}
+		}
+	}
+
+	/**
+	 * Read the configuration properties for the example.
+	 * 
+	 * @return the configuration object
+	 */
+	public static ConnectionProperties loadProperties() throws IOException {
+		String propsName = "connection.properties";
+
+		InputStream propsStream = openStream(propsName);
+		if (propsStream == null)
+			throw new IOException("Could not read properties " + propsName);
+
+		Properties props = new Properties();
+		props.load(propsStream);
+
+		return new ConnectionProperties(props);
+	}
+
+	/**
+	 * Read a resource for an example.
+	 * 
+	 * @param fileName
+	 *            the name of the resource
+	 * @return an input stream for the resource
+	 * @throws IOException
+	 */
+	public static InputStream openStream(String fileName) throws IOException {
+		return Util.class.getClassLoader().getResourceAsStream(fileName);
+	}
+	
+	/**
+	 * Configures proxy settings within the JVM. 
+	 * 
+	 * @param props connection configuration object
+	 */
+	public static void setProxy(String proxyHost, String proxyPort) {
+		
+		System.out.println("[INFO] Using proxy: " + proxyHost + ":" + proxyPort + ".");
+		
+		System.setProperty("https.proxyHost", proxyHost);
+        System.setProperty("https.proxyPort", proxyPort);
+        
+        System.setProperty("http.proxyHost", proxyHost);
+        System.setProperty("http.proxyPort", proxyPort);
+        
+	}
+
+}
