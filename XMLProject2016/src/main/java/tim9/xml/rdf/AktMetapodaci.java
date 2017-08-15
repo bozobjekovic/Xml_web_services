1e8fcab3ae59a757095d6de5bb86c4c53f516fd1
majamiljic <majamiljic2@gmail.com>
2017-08-15 20:47:51 +0200

    Done some work for metadata

---
+++ XMLProject2016/src/main/java/tim9/xml/rdf/AktMetapodaci.java
@@ -0,0 +1,52 @@
+package tim9.xml.rdf;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.DatabaseClientFactory;
+import com.marklogic.client.io.DOMHandle;
+import com.marklogic.client.semantics.GraphManager;
+import com.marklogic.client.semantics.RDFMimeTypes;
+
+import tim9.xml.util.Util;
+import tim9.xml.util.Util.ConnectionProperties;
+
+
+public class AktMetapodaci {
+	
+	private static DatabaseClient client;
+	
+	public static String getMetaData(ConnectionProperties props, String aktId) throws IOException {
+		
+		// Initialize the database client
+		if (props.database.equals("")) {
+			System.out.println("[INFO] Using default database.");
+			client = DatabaseClientFactory.newClient(props.host, props.port, props.user, props.password, props.authType);
+		} else {
+			System.out.println("[INFO] Using \"" + props.database + "\" database.");
+			client = DatabaseClientFactory.newClient(props.host, props.port, props.database, props.user, props.password, props.authType);
+		}
+		
+		String graphName = "akti/metadata/" + aktId;  //PROVERITI
+		
+		GraphManager graphManager = client.newGraphManager();
+		graphManager.setDefaultMimetype(RDFMimeTypes.NTRIPLES);
+		
+		DOMHandle domHandle = new DOMHandle();
+		
+		graphManager.read(graphName, domHandle).withMimetype(RDFMimeTypes.RDFXML);
+		
+		OutputStream out = new ByteArrayOutputStream();
+		
+		DOMUtil.transform(domHandle.get(), out);
+		return out.toString();
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		getMetaData(Util.loadProperties(), ".ana");
+	}
+
+}
