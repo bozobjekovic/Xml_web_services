ab37f72f1fecfcaeca3f2f00c6b31fa7d0759757
anamarojevic <bozo.bjekovic@gmail.com>
2017-08-30 00:56:11 +0200

    Form for adding akt done

--- XMLProject2016/static/angular/controller/dodaj-akt-controller.js
+++ XMLProject2016/static/angular/controller/dodaj-akt-controller.js
@@ -126,6 +126,421 @@
 							action : Xonomy.deleteElement
 						} ],
 						mustBeAfter : [ "NazivOrgana" ]
+					},
+					
+					// ########################################
+					// ------------ Deo elements -------------
+					"Deo" : {
+						menu : [
+							{
+								caption : "Dodaj novu <Glava>",
+								action : Xonomy.addGlavaChildElement,
+								actionParameter : "<Glava/>",
+								hideIf : function(jsElement) {
+									return jsElement.hasChildElement("Član");
+								}
+
+							}, {
+								caption : "Dodaj novi <Član>",
+								action : Xonomy.addClanChildElement,
+								actionParameter : "<Član/>",
+								hideIf : function(jsElement) {
+									return jsElement.hasChildElement("Glava");
+								}
+							}, {
+								caption : "Dodaj @naslov",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "naslov",
+									value : ""
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("naslov");
+								}
+							}, {
+								caption : "Dodaj @redniBroj",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "redniBroj",
+									value : "DEO "
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("redniBroj");
+								}
+							}, {
+								caption : "Obriši",
+								action : Xonomy.deleteElement
+							}
+						],
+						attributes : {
+							"naslov" : {
+								asker : Xonomy.askString
+							},
+							"redniBroj" : {
+								asker : Xonomy.askString
+							}
+						},
+						mustBeAfter : [ "Korisnik", "naslov" ]
+					},
+					
+					"redniBroj" : {
+						hasText : true,
+						menu : [ {
+							caption : "Obriši",
+							action : Xonomy.deleteElement
+
+						} ]
+					},
+					
+					// ########################################
+					// ----------- Glava elements -------------
+					"Glava" : {
+						menu : [
+							{
+								caption : "Dodaj novi <Odeljak>",
+								action : Xonomy.addOdeljakChildElement,
+								actionParameter : "<Odeljak/>",
+								hideIf : function(jsElement) {
+									return jsElement.hasChildElement("Član");
+								}
+
+							}, {
+								caption : "Dodaj novi <Član>",
+								action : Xonomy.addClanChildElement,
+								actionParameter : "<Član/>",
+								hideIf : function(jsElement) {
+									return jsElement.hasChildElement("Odeljak");
+								}
+							}, {
+								caption : "Dodaj @naslov",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "naslov",
+									value : ""
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("naslov");
+								}
+							}, {
+								caption : "Dodaj @redniBroj",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "redniBroj",
+									value : "Glava "
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("redniBroj");
+								}
+							}, {
+								caption : "Obriši",
+								action : Xonomy.deleteElement
+							}
+						],
+						attributes : {
+							"naslov" : {
+								asker : Xonomy.askString
+							},
+							"redniBroj" : {
+								asker : Xonomy.askString
+							}
+						}
+					},
+					
+					// ########################################
+					// --------- Odeljak elements -----------
+					"Odeljak" : {
+						menu : [
+							{
+								caption : "Dodaj novi <Pododeljak>",
+								action : Xonomy.addPododeljakChildElement,
+								actionParameter : "<Pododeljak/>",
+								hideIf : function(jsElement) {
+									return jsElement.hasChildElement("Član");
+								}
+							}, {
+								caption : "Dodaj novi <Član>",
+								action : Xonomy.addClanChildElement,
+								actionParameter : "<Član/>",
+								hideIf : function(jsElement) {
+									return jsElement.hasChildElement("Pododeljak");
+								}
+							}, {
+								caption : "Dodaj @naslov",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "naslov",
+									value : ""
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("naslov");
+								}
+							}, {
+								caption : "Dodaj @redniBroj",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "redniBroj",
+									value : ""
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("redniBroj");
+								}
+							}, {
+								caption : "Obriši",
+								action : Xonomy.deleteElement
+							}
+						],
+						attributes : {
+							"naslov" : {
+								asker : Xonomy.askString
+							},
+							"redniBroj" : {
+								asker : Xonomy.askString
+							}
+						}
+					},
+					
+					// ########################################
+					// -------- Pododeljak elements ----------
+					"Pododeljak" : {
+						menu : [
+							{
+								caption : "Dodaj novi <Član>",
+								action : Xonomy.addClanChildElement,
+								actionParameter : "<Član/>"
+							}, {
+								caption : "Dodaj @naslov",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "naslov",
+									value : ""
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("naslov");
+								}
+							}, {
+								caption : "Dodaj @redniBroj",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "redniBroj",
+									value : ""
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("redniBroj");
+								}
+							}, {
+								caption : "Obriši",
+								action : Xonomy.deleteElement
+							}
+						],
+						attributes : {
+							"naslov" : {
+								asker : Xonomy.askString
+							},
+							"redniBroj" : {
+								asker : Xonomy.askString
+							}
+						}
+					},
+					
+					// ########################################
+					// ----------- Clan elements ------------
+					"Član" : {
+						menu : [
+							{
+								caption : "Dodaj novi <Stav>",
+								action : Xonomy.addStavChildElement,
+								actionParameter : "<Stav/>",
+							}, {
+								caption : "Dodaj @naslov",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "naslov",
+									value : ""
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("naslov");
+								}
+							}, {
+								caption : "Dodaj @redniBroj",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "redniBroj",
+									value : "Clan "
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("redniBroj");
+								}
+							}, {
+								caption : "Obriši",
+								action : Xonomy.deleteElement
+							}
+						],
+						attributes : {
+							"naslov" : {
+								asker : Xonomy.askString
+							},
+							"redniBroj" : {
+								asker : Xonomy.askString
+							}
+						}
+					},
+					
+					// ########################################
+					// ----------- Stav elements ------------
+					"Stav" : {
+						menu : [
+							{
+								caption : "Dodaj novu <Tačka>",
+								action : Xonomy.addTackaChildElement,
+								actionParameter : "<Tačka/>"
+							}, {
+								caption : "Dodaj novi <Sadrzaj>",
+								action : Xonomy.newElementChild,
+								actionParameter : "<Sadrzaj/>",
+								hideIf : function(jsElement) {
+									return jsElement.hasChildElement("Sadrzaj");
+								}
+							}, {
+								caption : "Obriši",
+								action : Xonomy.deleteElement
+							}
+						]
+					},
+					
+					// ########################################
+					// ----------- Tacka elements ------------
+					"Tačka" : {
+						menu : [
+							{
+								caption : "Dodaj novi <Podtačka>",
+								action : Xonomy.addPodtackaChildElement,
+								actionParameter : "<Podtačka/>",
+							}, {
+								caption : "Dodaj novi <Sadrzaj>",
+								action : Xonomy.newElementChild,
+								actionParameter : "<Sadrzaj/>",
+								hideIf : function(jsElement) {
+									return jsElement.hasChildElement("Sadrzaj");
+								},
+							}, {
+								caption : "Dodaj @redniBroj",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "redniBroj",
+									value : ""
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("redniBroj");
+								}
+							}, {
+								caption : "Obriši",
+								action : Xonomy.deleteElement
+							}
+						],
+						mustBeAfter : [ "Sadrzaj" ],
+						attributes : {
+							"redniBroj" : {
+								asker : Xonomy.askString
+							}
+						}
+					},
+					
+					// ########################################
+					// --------- Podtacka elements -----------
+					"Podtačka" : {
+						menu : [
+							{
+								caption : "Dodaj novu <Alineja>",
+								action : Xonomy.addAlinejaChildElement,
+								actionParameter : "<Alineja/>"
+							}, {
+								caption : "Dodaj novi <Sadrzaj>",
+								action : Xonomy.newElementChild,
+								actionParameter : "<Sadrzaj/>",
+								hideIf : function(jsElement) {
+									return jsElement.hasChildElement("Sadrzaj");
+								}
+							}, {
+								caption : "Dodaj @redniBroj",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "redniBroj",
+									value : ""
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("redniBroj");
+								}
+							}, {
+								caption : "Obriši",
+								action : Xonomy.deleteElement
+							}
+						],
+						attributes : {
+							"redniBroj" : {
+								asker : Xonomy.askString
+							}
+						}
+					},
+					
+					// ########################################
+					// --------- Alineja elements -----------
+					"Alineja" : {
+						menu : [
+							{
+								caption : "Dodaj novi <Sadrzaj>",
+								action : Xonomy.newElementChild,
+								actionParameter : "<Sadrzaj/>"
+							}, {
+								caption : "Obriši",
+								action : Xonomy.deleteElement
+							}
+						]
+					},
+					
+					"Sadrzaj" : {
+						hasText : true,
+						menu : [
+							{
+								caption : "Obriši",
+								action : Xonomy.deleteElement
+							}
+						],
+						inlineMenu : [
+							{
+								caption : "Dodaj <Referenca>",
+								action : Xonomy.wrap,
+								actionParameter : {
+									template : "<Referenca>$</Referenca>",
+									placeholder : "$"
+								}
+							}
+						],
+						mustBeBefore : [ "Tačka", "Podtačka", "Alineja" ]
+					},
+					
+					"Referenca" : {
+						hasText : false,
+						menu : [ 
+							{
+								caption : "Obriši <Referenca>",
+								action : Xonomy.unwrap
+							}, {
+								caption : "Dodaj @URL",
+								action : Xonomy.newAttribute,
+								actionParameter : {
+									name : "URL",
+									value : "akt"
+								},
+								hideIf : function(jsElement) {
+									return jsElement.hasAttribute("URL");
+								}
+							}
+						],
+						attributes : {
+							"URL" : {
+								asker : Xonomy.askString
+							}
+						}
 					}
 				}
 			};
